45fd2b3153635fe10da058402af35a2339b86015
Ronit Herzog <lee.herzog5@gmail.com>
2018-04-10 16:01:48 +0300

    first step

--- main.js
+++ main.js
@@ -6,7 +6,15 @@ var reservations = {
 var name = prompt('Please enter the name for your reservation');
 
 var claimReservation = function () {
-  // write your code here!
+  if (name in reservations && reservations[name]["claimed"] === false){
+    alert("Welcome!");
+  }
+  else if (name in reservations && reservations[name]["claimed"] === true){
+    alert("Reservation already claimed!");
+  }
+  else {
+    alert("No reservation!");
+  }
 }
 
 claimReservation();
